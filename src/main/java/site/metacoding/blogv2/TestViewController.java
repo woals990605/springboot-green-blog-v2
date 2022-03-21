0052a87f72abc339153f11175d8631c64385036f
woals990605 <woals990605@naver.com>
2022-03-21 16:42:13 +0900

    3.Entity User, Post 생성 완료

--- src/main/java/site/metacoding/blogv2/TestViewController.java
+++ src/main/java/site/metacoding/blogv2/TestViewController.java
@@ -7,42 +7,42 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class TestViewController {
     // 모든 페이지 잘 작동하는지 확인 후 푸시 - 2. 컨트롤러 페이지 작동 테스트
 
-    @GetMapping("/post/writeForm")
+    @GetMapping("/test/post/writeForm")
     public String postWriteForm() {
         return "post/writeForm";
     }
 
-    @GetMapping("/post/list")
+    @GetMapping("/test/post/list")
     public String postList() {
         return "post/list";
     }
 
-    @GetMapping("/post/detail")
+    @GetMapping("/test/post/detail")
     public String postDetail() {
         return "post/detail";
     }
 
-    @GetMapping("/post/updateForm")
+    @GetMapping("/test/post/updateForm")
     public String postUpdateForm() {
         return "post/updateForm";
     }
 
-    @GetMapping("/joinForm")
+    @GetMapping("/test/joinForm")
     public String userJoinForm() {
         return "user/joinForm";
     }
 
-    @GetMapping("/loginForm")
+    @GetMapping("/test/loginForm")
     public String userLoginForm() {
         return "user/loginForm";
     }
 
-    @GetMapping("/user")
+    @GetMapping("/test/user")
     public String userDetail() {
         return "user/detail";
     }
 
-    @GetMapping("/user/updateForm")
+    @GetMapping("/test/user/updateForm")
     public String userUpdateForm() {
         return "user/updateForm";
     }
---
+++ src/main/java/site/metacoding/blogv2/domain/user/User.java
@@ -0,0 +1,47 @@
+package site.metacoding.blogv2.domain.user;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@EntityListeners(AuditingEntityListener.class) // 시간관련
+@Entity // 가장 핵심적인 어노테이션 밑으로
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, nullable = false, length = 12)
+    private String username;
+
+    @Column(nullable = false, length = 12)
+    private String password;
+
+    @Column(nullable = false, length = 30)
+    private String email;
+
+    @Column(nullable = false, length = 300)
+    private String addr; // API 주소 라이브러리 사용할 예정
+
+    @CreatedDate // insert 할때만 동작
+    private LocalDateTime createDate;
+    @LastModifiedDate // update 할때만 동작
+    private LocalDateTime updateDate;
+}
